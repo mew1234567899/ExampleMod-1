a4e3a774a6e9cccee88a8ba1fc356cb0caeeac8b
mew1234567899 <iamaperson123thetwelve@gmail.com>
2021-02-20 23:26:55 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -25,7 +25,12 @@ require("units/testunit4");
 
 print("Load Complete!");
 
-
+//this should export a map into sector 174//
+const mapVariable = extend(SectorPreset, "mewsmap", Planets.serpulo, 174, {
+    localizedName: "sadland",
+    description: "you'll never laugh again",
+    difficulty: 2
+})
 
 
 
